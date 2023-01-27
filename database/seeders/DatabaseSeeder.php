bf8cba1ae1640be6a4e728507604dfe66098541e
lynxiel <Barce!o1086>
2023-01-27 09:04:01 +0300

    demouser generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Demouser',
+             'email' => 'test@example.com',
+             'api_token'=>'227D1F7CQ0o7LutSfia4xYlC3btJ8Qy1nmmb5PkKLNeytvg6OL2zwuVQ6IIn',
+         ]);
     }
 }
